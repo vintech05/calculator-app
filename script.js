9ad6a2c57b6b090803903d8e8d1486d4e6d39da6
vintech05 <kelanjutannyakelvin@gmail.com>
2024-05-02 21:56:28 +0700

    fixed operator function

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const displayScreen = document.querySelector(".display-screen");
-const keys = document.querySelectorAll(".key");
+const displayScreen = document.querySelector(".screen");
 
-keys.forEach((key) => {
-  key.addEventListener("click", () => {
-    if (key.innerText === "RESET") {
-      displayScreen.innerText = "";
-    } else if (key.innerText === "DEL") {
-      displayScreen.innerText = displayScreen.innerText.slice(0, -1);
-    } else if (key.innerText === "=") {
-      displayScreen.innerText = eval(displayScreen.innerText);
-    } else {
-      displayScreen.innerText += key.innerText;
-    }
-  });
-});
+function resetKey() {
+  displayScreen.value = "";
+}
+
+function deleteKey() {
+  displayScreen.value = displayScreen.value.slice(0, -1);
+}
+
+function appendNumber(input) {
+  displayScreen.value += input;
+}
+
+function showResult() {
+  try {
+    displayScreen.value = eval(displayScreen.value);
+  } catch (error) {
+    displayScreen.value = "Error";
+  }
+}
