e11c744517a7ae834e062129475a6e0d53ae3084
vintech05 <kelanjutannyakelvin@gmail.com>
2024-05-06 20:26:28 +0700

    fixed bug when 'null' gets stored inside the body tag class

--- script.js
+++ script.js
@@ -88,6 +88,12 @@ function showResult() {
 }
 
 function onLoad() {
+  if (typeof localStorage.getItem("theme") === "undefined") {
+    document.body.className = localStorage.getItem("theme") || "theme-1";
+    displayScreen.value = localStorage.getItem("displayScreen") || "";
+    displayScreen.style.fontSize =
+      localStorage.getItem("displayScreenSize") || "2.5rem";
+  }
   document.body.className = localStorage.getItem("theme");
   displayScreen.value = localStorage.getItem("displayScreen");
   displayScreen.style.fontSize = localStorage.getItem("displayScreenSize");
